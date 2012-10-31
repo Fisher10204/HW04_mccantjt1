6a288d6a862a26c6be0426bb398b024429102f80
Jacob McCanty <mccantjt@muohio.edu>
2012-10-30 20:55:17 -0400

    Fixed the files
    The previously moved files had had all the code somehow removed, the
    code that was in the files has now been replaced ans is now finding the
    Starbucks.h file.

---
+++ include/Entry.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Starbucks.h"
+#include <string>
+using namespace std;
+
+class mccantjtEntry : public Entry{
+public:
+	mccantjtEntry();
+	mccantjtEntry(string name, double x, double y);
+	double getX();
+	double getY();
+	void setX(double x);
+	void setY(double y);
+	void setName(string name);
+	string getName();
+private:
+	string name;
+	double x;
+	double y;
+}
