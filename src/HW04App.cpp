9908c4ae80d9c25f5f50327baa4474f2c6932a2d
Jacob McCanty <mccantjt@muohio.edu>
2012-11-12 11:45:31 -0500

    Adding visualization code, doesn't work yet

--- src/HW04App.cpp
+++ src/HW04App.cpp
@@ -3,6 +3,8 @@
 #pragma once
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/ImageIo.h"
 #include "Starbucks.h"
 #include "mccantjtStarbucks.h"
 #include "cinder/Rand.h"
